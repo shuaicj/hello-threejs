55729aab7c5be83b7c9c632d24cfe20740d78a15
shuaicj <shuaicj@gmail.com>
2017-01-13 15:15:38 +0800

    change floor to solid

--- floor/floor.js
+++ floor/floor.js
@@ -1,22 +1,12 @@
 function Floor() {
-    this.view = new THREE.Group();
     this.radius = 10;
     this.color = 0x888888;
-    for (var x = -this.radius; x <= this.radius; x++) {
-        this.drawLine(x, -this.radius, x, this.radius);
-    }
-    for (var y = -this.radius; y <= this.radius; y++) {
-        this.drawLine(-this.radius, y, this.radius, y);
-    }
-}
-
-Floor.prototype.drawLine = function(x0, y0, x1, y1) {
-    var material = new THREE.LineBasicMaterial({color: this.color});
-    var geometry = new THREE.Geometry();
-    var v0 = new THREE.Vector3(x0, y0, -0.01);
-    var v1 = new THREE.Vector3(x1, y1, -0.01);
-    geometry.vertices.push(v0, v1);
-    this.view.add(new THREE.Line(geometry, material));
+    this.view = new THREE.Mesh(
+        new THREE.PlaneGeometry(2 * this.radius, 2 * this.radius), 
+        new THREE.MeshPhongMaterial({color: this.color, side: THREE.DoubleSide})
+    );
+    this.view.position.z = -0.01;
+    this.view.receiveShadow = true;
 }
 
 Floor.prototype.addToWorld = function(world) {
